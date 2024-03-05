fffa8679df77f7b8809c3afe6ae414d8dd4aad93
qingyizhiyan <126797945+qingyizhiyan@users.noreply.github.com>
2024-03-05 15:12:38 +0800

    Update zhihu.py

--- zhihu.py
+++ zhihu.py
@@ -1,3 +1,5 @@
 import requests,re
 url = "https://www.zhihu.com"
-response = requests.get(url)
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763'}
+response = requests.get(url，headers=headers)
+print(response.text)
