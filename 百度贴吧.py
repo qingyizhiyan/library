e5aa3362a11381f37d88f400223551bae6211149
qingyizhiyan <126797945+qingyizhiyan@users.noreply.github.com>
2024-05-21 16:11:02 +0800

    Update 百度贴吧.py

--- 百度贴吧.py
+++ 百度贴吧.py
@@ -1,18 +1,6 @@
 import requests,re
 from bs4 import BeautifulSoup
-txt = ''
-url = input('the url:')
-cookies = input('the cookies:')
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763',
-           'cookie':cookies if cookies else ''}
-try:
-    response = requests.get(url,headers=headers,timeout=60)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print("FINISHED")
-    txt = soup.prettify()
-except:
-    print("FAILED")
-def pa():
+def pa(txt):
     pattern = (r'<a rel="noopener" href="/p/[0-9]+" title="(.+?)" target="_blank" class="j_th_tit ">.+?</a>'
             r'\s*</div><div class="threadlist_author pull_right">'
             r'\s*<span class="tb_icon_author "'
@@ -29,3 +17,14 @@ def pa():
     l = re.findall(pattern,txt)
     for i in l:
         print(i[0],"\n","作者: "+i[1],"\n","时间: "+i[2],"\n",i[3],"\n",sep='')
+cookies = input('the cookies:')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763',
+           'cookie':cookies if cookies else ''}
+for number in range(0,1001,50):
+    url = 'https://tieba.baidu.com/f?kw=%E5%8E%A6%E9%97%A8%E5%A4%A7%E5%AD%A6&ie=utf-8&pn='+str(number)
+    try:
+        response = requests.get(url,headers=headers,timeout=60)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        pa(soup.prettify())
+    except:
+        print("FAILED")
